e219440af35022f074e746b11de184e17c131f92
abhisheksingh75 <44764827+abhisheksingh75@users.noreply.github.com>
2019-09-07 05:25:41 +0530

    Update Set_Matrix_Zeros.py

--- Arrays/Set_Matrix_Zeros.py
+++ Arrays/Set_Matrix_Zeros.py
@@ -20,43 +20,50 @@ class Solution:
     # @param A : list of list of integers
     # @return the same list modified
     def setZeroes(self, A):
-        rowFlag,colFlag = 'N', 'N'
         noOfRow = len(A)
-        noOfCol = len(A[0])
-        
-        #traverse first row
-        for j in range(noOfCol):
-            if A[0][j] == 0 or A[0][j] == 'Y': 
-                A[0][j] = 'Y'            
-                rowFlag = 'Y'
-                
-        #traverse first column
-        for i in range(noOfRow):
-            if A[i][0] == 0 or  A[i][0] == 'Y':
-                A[i][0] = 'Y'
-                colFlag = 'Y'
-                
-        
-        for i in range(1, noOfRow):
-            for j in range(1, noOfCol):
-                if A[i][j] == 0:
-                    A[i][0] = 'Y'
-                    A[0][j] = 'Y'
-        #start putting Zeros            
-        for i in range(1, noOfRow):
-            for j in range(1, noOfCol):
-                if A[i][0] == 'Y' or A[0][j] == 'Y':
-                   A[i][j] = 0
-               
-        #traverse first row
-        for j in range(noOfCol):
-            if rowFlag == 'Y' or A[0][j] == 'Y' :
-                A[0][j] = 0
+        noOfCol  = len(A[0])
+        rowflag, colflag = 1,1
+        #check if 1st row is having zero's 
+        for i in range(noOfCol):
+            if A[0][i]  == 0:
+                rowflag = 0
+                break
             
-                
-        #traverse first column
+        #check if 1st column is having zero's    
         for i in range(noOfRow):
-            if colFlag == 'Y' or A[i][0] == 'Y':
+            if A[i][0]  == 0:
+                colflag = 0
+                break
+        
+        #traverse the matrix and mark that row and column corresponding to element zero    
+        for i in range(1, noOfRow,1):
+            for j in range(1, noOfCol,1):
+                if A[i][j] == 0:
+                    A[i][0] = 0
+                    A[0][j] = 0
+        
+        #populate the matrix which having row and  column marked to zero
+        for i in range(1, noOfRow,1):
+            for j in range(1, noOfCol,1):
+                if A[i][0] == 0 or A[0][j] == 0:
+                    A[i][j] = 0
+                    
+                    
+        #if rowflag is set mark whole row as zero
+        if rowflag == 0:
+            for i in range(noOfCol):
+                A[0][i] = 0
+
+        #if colflag is set mark whole row as zero
+        if colflag == 0:
+            for i in range(noOfRow):
                 A[i][0] = 0
-                
+                    
         return A
+                    
+                    
+                    
+                    
+                
+                
+     
