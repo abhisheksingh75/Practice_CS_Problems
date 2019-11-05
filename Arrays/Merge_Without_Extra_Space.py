e72e20051fb9e670221e9be902f10bd4cd7068e8
abhisheksingh75 <44764827+abhisheksingh75@users.noreply.github.com>
2019-11-05 17:09:38 +0530

    Create Merge_Without_Extra_Space.py

---
+++ Arrays/Merge_Without_Extra_Space.py
@@ -0,0 +1,75 @@
+"""
+Given two sorted arrays arr1[] and arr2[] in non-decreasing order with size n and m. The task is to merge the two sorted arrays into one sorted array (in non-decreasing order).
+
+Note: Expected time complexity is O((n+m) log(n+m)). DO NOT use extra space.  We need to modify existing arrays as following.
+
+Input: arr1[] = {10};
+       arr2[] = {2, 3};
+Output: arr1[] = {2}
+        arr2[] = {3, 10}  
+
+Input: arr1[] = {1, 5, 9, 10, 15, 20};
+       arr2[] = {2, 3, 8, 13};
+Output: arr1[] = {1, 2, 3, 5, 8, 9}
+        arr2[] = {10, 13, 15, 20} 
+Input:
+First line contains an integer T, denoting the number of test cases. First line of each test case contains two space separated integers X and Y, denoting the size of the two sorted arrays. Second line of each test case contains X space separated integers, denoting the first sorted array P. Third line of each test case contains Y space separated integers, denoting the second array Q.
+
+Output:
+For each test case, print (X + Y) space separated integer representing the merged array.
+
+Constraints:
+1 <= T <= 100
+1 <= X, Y <= 5*104
+0 <= arr1i, arr2i <= 109
+
+Example:
+Input:
+2
+4 5
+1 3 5 7
+0 2 6 8 9
+2 3
+10 12
+5 18 20
+
+Output:
+0 1 2 3 5 6 7 8 9
+5 10 12 18 20
+
+Explanation:
+Testcase 1: After merging two non-decreasing arrays, we have, 0 1 2 3 5 6 7 8 9
+"""
+#code
+
+class Solution:
+    def solve(self,arr1,arr2):
+        ptr1, ptr2 = len(arr1)-1, 0
+        while(ptr1 >= 0 and ptr2<len(arr2) and arr1[ptr1]>arr2[ptr2]):
+            arr1[ptr1], arr2[ptr2] = arr2[ptr2], arr1[ptr1]
+            ptr1 -=1 
+            ptr2 += 1
+        arr1.sort()
+        arr2.sort()
+        arr1.extend(arr2)
+        arr1 = [str(arr1[i]) for i in range(len(arr1))]
+        string = " ".join(arr1)
+        print(string)
+        
+        
+
+
+sol = Solution()
+testcases = input()
+input_list = []
+for i in range(int(testcases)):
+    list_size = input()
+    arr1 = list(map(int,input().split()))
+    arr2 = list(map(int,input().split()))
+    input_list.append([arr1, arr2])
+
+for arr1, arr2 in input_list:
+    sol.solve(arr1,arr2)
+    
+    
+    
